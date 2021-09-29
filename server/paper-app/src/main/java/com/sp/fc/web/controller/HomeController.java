2bc0ecd55c37db764ef422f42a221b264ac7c295
songwoo <thddn191@gmail.com>
2021-09-30 04:47:45 +0900

    Set index, loginForm UI
    첫 화면과 학생,선생,관리자 로그인에 따른 화면 분기 및 UI분기

---
+++ server/paper-app/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.sp.fc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("name", "world ! by ThymeLeaf");
+        return "index";
+    }
+
+    @GetMapping("/login") // /login?site=teacher 처럼 분기하기
+    public String login(@RequestParam String site, Model model) {
+        model.addAttribute("site", site);
+        return "loginForm";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(@RequestParam String site, Model model) {
+        model.addAttribute("site", site);
+        return "redirect:/"+site;
+    }
+}
